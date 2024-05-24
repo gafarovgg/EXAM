574ae65fa77833babae61abfd80b1cf7730fcec7
gafarovgg <mi6whu2j9@code.edu.az>
2024-05-24 08:23:03 +0400

    routing

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Route, Routes } from "react-router";
+import Header from "./layouts/Header";
+import Footer from "./layouts/Footer";
+import HomePage from "./pages/HomePage";
+import AddPage from "./pages/AddPage";
+import DetailsPage from "./pages/DetailsPage";
+import ErrorPage from "./pages/ErrorPage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/add-product" element={<AddPage />} />
+        <Route path="/add-product/:id" element={<DetailsPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
